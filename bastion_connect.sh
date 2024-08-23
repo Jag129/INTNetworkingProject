cf239e0fff9784e0b3aaab39ff6e5e815376b227
jess <yisrael129@gmail.com>
2024-08-23 13:25:10 +0300

    Testing 1 2 3

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Assign arguments to variables
 bsto="$1"
 rem="$2"
@@ -31,47 +30,7 @@ fi
 if [ "$#" -eq 3 ]; then
   echo "Command accepted,"
       #ssh -i "$KEY_PATH" -o ProxyCommand="ssh -W %h:%p -i $KEY_PATH ubuntu@$bsto" ubuntu@$rem "$cmd"
-      ssh -t -i $KEY_PATH ubuntu@$bsto ssh -i $remkey ubuntu@$rem "$cmd"
+      ssh -t -i $KEY_PATH ubuntu@$bsto ssh -i "$remkey" ubuntu@$rem "$cmd"
       else
         exit 1
 fi
-
-/'
-#!/bin/bash
-
-# Assign arguments to variables
-bsto="$1"
-rem="$2"
-cmd="$3"
-
-# Check if KEY_PATH is set
-if [ -z "$KEY_PATH" ]; then
-    echo "KEY_PATH env var is expected."
-    exit 5
-fi
-
-# Check if at least one argument is provided
-if [ "$#" -lt 1 ]; then
-    echo "Please provide bastion IP address."
-    exit 5
-fi
-
-# Case 1: Connect directly to the public instance
-if [ "$#" -eq 1 ]; then
-    echo "Connecting directly to the public instance..."
-    ssh -i "$KEY_PATH" ubuntu@$bsto
-
-# Case 2: Connect to the private instance via the public instance
-elif [ "$#" -eq 2 ]; then
-    echo "Connecting to the private instance via the public instance..."
-    ssh -i "$KEY_PATH" -o ProxyCommand="ssh -W %h:%p -i $KEY_PATH ubuntu@$bsto" ubuntu@$rem
-
-# Case 3: Run a command on the private instance through the public instance
-elif [ "$#" -eq 3 ]; then
-    echo "Command accepted, running command on the private instance via the public instance..."
-    ssh -i "$KEY_PATH" -o ProxyCommand="ssh -W %h:%p -i $KEY_PATH ubuntu@$bsto" ubuntu@$rem "$cmd"
-else
-    echo "Incorrect usage."
-    exit 5
-fi
-/'
