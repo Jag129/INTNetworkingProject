615e779151332f96c379ad1f40cb741b52d78e1a
jess <yisrael129@gmail.com>
2024-08-26 02:35:50 +0300

    After changing

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -11,9 +11,20 @@ if [ "$#" -lt 1 ]; then
     echo "Usage: $0 <private-instance-ip>"
     exit 1
 fi
+
+#Generate New Keys
 ssh-keygen -t rsa -b 4096 -f $newkey
-ssh -i $exkey ubuntu@$input 'echo "$newpub" >> /home/ubuntu/.ssh/authorized_hosts'
+
+#ssh -i $exkey ubuntu@$input 'echo "$newpub" >> /home/ubuntu/.ssh/authorized_hosts'
+#Append the current key
 ssh -i $exkey ubuntu@$input 'cat >> /home/ubuntu/.ssh/authorized_hosts' < $newpub
+
+#Delete old Key
+local_key=$(cat /path/to/local/key.pub)
+
+ssh -i $exkey ubuntu@$input "
+    sed -i \"/$exkey/d\" ~/.ssh/authorized_keys
+"
 
 
 
